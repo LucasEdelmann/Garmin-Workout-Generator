3a2af90a184b9808ab32b3e4e19d5e1666b2410b
Lucas <32237839+LucasEdelmann@users.noreply.github.com>
2025-04-17 23:49:35 +0200

    Update script.js - fixing

--- frontend/script.js
+++ frontend/script.js
@@ -12,7 +12,10 @@ document.getElementById('upload-form').addEventListener('submit', async (event) 
 
     const result = await response.json();
     if (response.ok) {
-        document.getElementById('response').innerHTML = `TCX-Datei erstellt! <a href="/static/training_plan.tcx" download class="text-blue-500 hover:underline">Download</a>`;
+        const downloadLink = document.getElementById('download-link');
+        downloadLink.href = `/static/${result.tcx_file_path}`;  // Korrekte URL zur Datei
+        downloadLink.classList.remove('hidden');
+        document.getElementById('response').innerHTML = `TCX-Datei erstellt! <a href="${downloadLink.href}" download class="text-blue-500 hover:underline">Download</a>`;
     } else {
         document.getElementById('response').innerHTML = `Fehler: ${result.message}`;
     }
